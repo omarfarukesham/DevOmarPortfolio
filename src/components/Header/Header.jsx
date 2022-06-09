79d09e4453b274ec5348ddde5d59165f9ab1680b
Omar Faruk <omarfarukesham@gmail.com>
2022-06-09 09:43:18 +0600

    project setup done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,124 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-import CustomLink from '../CoustLink/CustomLink';
-import './Header.css'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
-    return (
-        <div className='nav'>
-            <nav>
-                <CustomLink className='navDesign' to="/" >Home</CustomLink>
-                <CustomLink className='navDesign' to="/About" >About</CustomLink>
-                <CustomLink className='navDesign' to="/Friends" >Friends</CustomLink>
-                {/* <Link className='navDesign' to="/">Home</Link>
-                <Link className='navDesign' to="/About">About</Link>
-                <Link className='navDesign' to="/Friends">Friends</Link> */}
-        
-            </nav>
-           
+
+    
+  return (
+    <>
+      <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li tabindex="0">
+                <a class="justify-between">
+                  Activity
+                  <svg
+                    class="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                  </svg>
+                </a>
+                <ul class="p-2">
+                  <li>
+                    <NavLink to="/training">Training</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/bookReview">Book Review</NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <NavLink to="/blogs">Blogs</NavLink>
+              </li>
+              <li>
+                <NavLink to="/service">Service</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Contact">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/project">Project</NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <a class="btn btn-ghost normal-case text-xl">DevOmar</a>
         </div>
-    );
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="blogs">Blogs</NavLink>
+            </li>
+            <li>
+              <NavLink to="Projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="service">Service</NavLink>
+            </li>
+            <li tabindex="0">
+              <a>
+                Activity
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </a>
+              <ul class="p-2">
+                <li>
+                  <NavLink to="/training">Training</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/bookReview">Books Review</NavLink>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <NavLink to="about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
